0303566c925073c1da42c7580b2881ff9af3c0b6
Aridan Pantoja <aridanpantoja@gmail.com>
2025-01-29 12:43:03 -0300

    feat(ui): add accordion component and update sparkles and tailwind config
    - Add new accordion component in src/components/ui/accordion.tsx
    - Disable onClick event in SparklesCore component
    - Extend tailwind config with new animations and keyframes for accordion

--- src/components/ui/sparkles.tsx
+++ src/components/ui/sparkles.tsx
@@ -72,7 +72,7 @@ export const SparklesCore = (props: ParticlesProps) => {
             interactivity: {
               events: {
                 onClick: {
-                  enable: true,
+                  enable: false,
                   mode: 'push',
                 },
                 onHover: {
