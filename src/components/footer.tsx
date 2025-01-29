21b4e15d1c8adaec047a5105e7d171bdd49ee467
Aridan Pantoja <aridanpantoja@gmail.com>
2025-01-29 12:44:42 -0300

    feat(components): redesign footer and enhance navbar
    - Add new footer component with logo, navigation links, and credits
    - Improve navbar styling and responsiveness
    - Implement WidthWrapper and Icons components in footer
    - Adjust navbar layout and background for better mobile and desktop display

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,41 @@
+import { Icons } from '@/components/icons'
+import { WidthWrapper } from '@/components/width-wrapper'
+import { siteConfig } from '@/config'
+import Link from 'next/link'
+
 export function Footer() {
   return (
-    <footer>
-      <div>footer</div>
+    <footer className="space-y-6 text-center">
+      <WidthWrapper>
+        <div>
+          <div className="flex flex-col items-center gap-6">
+            <Link
+              href="/"
+              target="_top"
+              className="flex w-fit items-center justify-center"
+            >
+              <Icons.logo className="h-fit w-48 fill-foreground/80 transition-colors hover:fill-foreground/50" />
+            </Link>
+
+            <ul className="flex flex-wrap items-center justify-center gap-3 text-sm text-muted-foreground">
+              {siteConfig.mainNav.map((link) => (
+                <li
+                  key={link.label}
+                  className="transition-colors hover:text-foreground"
+                >
+                  <Link href={link.href}>{link.label}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </WidthWrapper>
+
+      <div className="border-t py-4 text-xs text-muted-foreground">
+        <WidthWrapper>
+          <p>Created by Karol Wojtyla, Aridan Pantoja</p>
+        </WidthWrapper>
+      </div>
     </footer>
   )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,10 +7,10 @@ import { Icons } from './icons'
 
 export function Navbar() {
   return (
-    <header className="sticky inset-x-0 top-0 z-50 border-b bg-background backdrop-blur supports-[backdrop-filter]:bg-background/60 md:fixed md:top-6 md:border-none md:bg-transparent md:backdrop-blur-0">
+    <header className="sticky inset-x-0 top-0 z-50 w-full border-b bg-background backdrop-blur supports-[backdrop-filter]:bg-background/60 md:fixed md:top-6 md:border-none md:bg-transparent md:backdrop-blur-none">
       <WidthWrapper className="max-w-prose">
-        <div className="mx-auto md:bg-background md:backdrop-blur md:supports-[backdrop-filter]:bg-background/60">
-          <nav className="flex h-14 items-center justify-between gap-8 rounded-full px-0 md:border md:px-8">
+        <div className="mx-auto">
+          <nav className="flex h-14 items-center justify-between gap-8 rounded-full px-0 md:border md:bg-background md:px-8 md:backdrop-blur md:supports-[backdrop-filter]:bg-background/60">
             <Link href="/" className="flex items-center">
               <Icons.logo className="h-fit w-16 translate-y-1 fill-[#f087d9]" />
             </Link>
